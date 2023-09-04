bb7f79954cb62da61d313b62054f9392e284856b
Victor Mafra <victorvmafra97@gmail.com>
2023-09-04 15:13:50 -0300

    add swagger/adjust health path

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('DrivenPass - Rest API')
+    .setDescription('API for DrivenPass, with authorization, cards, credentials, notes and erase paths')
+    .setVersion('1.0')
+    .addTag('drivenpass')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
 
   const port = process.env.PORT || 3000
   await app.listen(port, () => {
